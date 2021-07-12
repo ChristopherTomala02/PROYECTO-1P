0d3c4a49bd09ec439f2f9ba445647d8e9fe3b868
yanaleenpluas <85530151+yanaleenpluas@users.noreply.github.com>
2021-07-11 23:41:41 -0500

    Proyecto

---
+++ Agente.java
@@ -0,0 +1,8 @@
+public class Agente{
+  private String cedula;
+  private int nroRespuestas;
+  private String correo;
+  private ArrayList<Cliente> listaVentas;
+  private String buzon;
+  private String codigo;
+  }
