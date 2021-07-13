90d5477fca929626c5a31d19430702a3cb244fa4
maza-issac8er01@outlook.com <maza-issac8er01@outlook.com>
2021-07-12 20:40:03 -0500

    Modificando en JAVA Propiedades

--- Propiedades.java
+++ Propiedades.java
@@ -1,10 +1,40 @@
-public class Propiedades{
-  private String CodigoPropiedades;
-  private int precio;
-  private String descripcion;
-  private double ancho;
-  private double profundida ;
-  private Agente AgenteVenta;
+package Proyecto;
 
 
+public class PROPIEDADES {
+    private double ancho;
+    private double largo;
+    private double valor;
+    private Ubicacion ubicacion;
+    private String Tipo_propiedad;
+    private String id;
+    private Agente AgenteVenta;
+    
+    public PROPIEDADES(double ancho, double largo, double valor, String id ){
+        ubicacion = new Ubicacion();
+        AgenteVenta = null;
+        this.ancho = ancho;
+        this.largo = largo;
+        this.valor = valor;
+        this.id = id;
+
+    }
+    
+    public PROPIEDADES(double ancho, double largo, double valor, String id , Agente AgenteVenta, Ubicacion ubicacion){
+        this.ubicacion = ubicacion;
+        this.AgenteVenta = AgenteVenta;
+        this.ancho = ancho;
+        this.largo = largo;
+        this.valor = valor;
+        this.id = id;
+    }
+    
+    public PROPIEDADES(double ancho, double largo, double valor, String id , Ubicacion ubicacion){
+        this.ubicacion = ubicacion;
+        AgenteVenta = null;
+        this.ancho = ancho;
+        this.largo = largo;
+        this.valor = valor;
+        this.id = id;
+    }
 }
