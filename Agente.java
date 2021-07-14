c1b819c3935fcdb8327652069bee65f666dbeb98
Christophoro Tomalá <85134776+ChristopherTomala02@users.noreply.github.com>
2021-07-14 15:50:40 -0500

    Update Agente.java

--- Agente.java
+++ Agente.java
@@ -1,8 +1,43 @@
+package Personajes;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author DELL
+ */
 public class Agente{
-  private String cedula;
   private int nroRespuestas;
-  private String correo;
   private ArrayList<Cliente> listaVentas;
-  private String buzon;
+  private Chat buzon;
   private String codigo;
-  }
+  
+  public Agente(String nomUsuario, String password, String ID, String correo) {
+        super(nomUsuario, password, ID, correo);
+        this.nroRespuestas = nroRespuestas;
+        this.listaVentas = listaVentas;
+        this.buzon= buzon;
+        this.codigo = codigo;
+      
+    }
+
+    public int getNroRespuestas() {
+        return nroRespuestas;
+    }
+
+    public ArrayList<Cliente> getListaVentas() {
+        return listaVentas;
+    }
+
+    public Chat getBuzon() {
+        return buzon;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+    @Override
+    public String toString() {
+        return "Agente{" + "nroRespuestas=" + nroRespuestas + ", listaVentas=" + listaVentas + ", buzon=" + buzon + ", codigo=" + codigo + '}';
+    }
+}
